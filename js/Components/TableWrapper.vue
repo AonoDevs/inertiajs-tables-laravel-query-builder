edf5e269e85b2233ec8acd4564524c6e41e41ea7
Roriwars <robin2b2010@gmail.com>
2025-04-01 18:24:16 +0200

    fix css

--- js/Components/TableWrapper.vue
+++ js/Components/TableWrapper.vue
@@ -2,7 +2,7 @@
   <div class="flow-root">
     <div class="overflow-x-auto">
       <div class="inline-block min-w-full align-middle">
-        <div class="overflow-hidden shadow-smring-1 ring-black/5 sm:rounded-lg">
+        <div class="overflow-hidden shadow-sm ring-1 ring-black/5 sm:rounded-lg">
           <slot />
         </div>
       </div>
