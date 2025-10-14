973fbd3456d4f5aac5385ebf8c055db5e7527d68
Roriwars <robin2b2010@gmail.com>
2025-10-14 12:10:13 +0200

    update layout with lucide and shadcn component - add search row and column selection

--- js/Components/TableAddSearchRow.vue
+++ js/Components/TableAddSearchRow.vue
@@ -1,50 +1,34 @@
 <template>
-  <ButtonWithDropdown
-    ref="dropdown"
-    dusk="add-search-row-dropdown"
-    :disabled="!hasSearchInputsWithoutValue"
-    class="w-auto"
-    :color="color"
-  >
-    <template #button>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5 text-gray-400"
-
-        viewBox="0 0 20 20"
-        fill="currentColor"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-          clip-rule="evenodd"
-        />
-      </svg>
-    </template>
-
-    <div
-      role="menu"
-      aria-orientation="horizontal"
-      aria-labelledby="add-search-input-menu"
-      class="min-w-max"
-    >
-      <button
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="outline">
+        <Search class="size-5" />
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent>
+      <DropdownMenuItem
         v-for="(searchInput, key) in searchInputs"
         :key="key"
         :dusk="`add-search-row-${searchInput.key}`"
-        class="text-left w-full px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-900 hover:text-gray-300"
         role="menuitem"
         @click.prevent="enableSearch(searchInput.key)"
       >
         {{ searchInput.label }}
-      </button>
-    </div>
-  </ButtonWithDropdown>
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
 </template>
 
 <script setup>
-import ButtonWithDropdown from "./ButtonWithDropdown.vue";
 import { ref } from "vue";
+import { Search } from "lucide-vue-next";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger
+} from "./ui/dropdown-menu/index.js";
+import { Button } from "./ui/button/index.js";
 
 const props = defineProps({
     searchInputs: {
@@ -64,7 +48,7 @@ const props = defineProps({
 
     color: {
         type: String,
-        default: 'primary',
+        default: "primary",
         required: false,
     },
 });
--- js/Components/TableColumns.vue
+++ js/Components/TableColumns.vue
@@ -1,80 +1,38 @@
 <template>
-  <ButtonWithDropdown
-    placement="bottom-end"
-    dusk="columns-dropdown"
-    :color="color"
-  >
-    <template #button>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5 text-gray-400"
-        viewBox="0 0 20 20"
-        fill="currentColor"
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="outline">
+        <Eye class="size-5" />
+        <span v-if="hasHiddenColumns">({{ hiddenColumnsCount }})</span>
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent>
+      <template
+        v-for="(column, key) in props.columns"
+        :key="key"
       >
-        <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-        <path
-          fill-rule="evenodd"
-          d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-          clip-rule="evenodd"
-        />
-      </svg>
-      <span v-if="hasHiddenColumns" class="ml-1">({{ hiddenColumnsCount }})</span>
-    </template>
-
-    <div
-      role="menu"
-      aria-orientation="horizontal"
-      aria-labelledby="toggle-columns-menu"
-      class="min-w-max"
-    >
-      <div class="px-2">
-        <ul class="divide-y divide-gray-200 dark:divide-gray-700">
-          <li
-            v-for="(column, key) in props.columns"
-            v-show="column.can_be_hidden"
-            :key="key"
-            class="py-2 flex items-center justify-between"
-          >
-            <p
-              class="text-sm text-gray-900 dark:text-gray-200"
-            >
-              {{ column.label }}
-            </p>
-
-            <button
-              type="button"
-              class="ml-4 relative inline-flex shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-light-blue-500"
-              :class="{
-                'bg-green-500': !column.hidden,
-                'bg-gray-200 dark:bg-gray-800': column.hidden,
-              }"
-              :aria-pressed="!column.hidden"
-              :aria-labelledby="`toggle-column-${column.key}`"
-              :aria-describedby="`toggle-column-${column.key}`"
-              :dusk="`toggle-column-${column.key}`"
-              @click.prevent="onChange(column.key, column.hidden)"
-            >
-              <span class="sr-only">Column status</span>
-              <span
-                aria-hidden="true"
-                :class="{
-                  'translate-x-5': !column.hidden,
-                  'translate-x-0': column.hidden,
-                }"
-                class="inline-block h-5 w-5 rounded-full bg-white dark:bg-gray-900 shadow-sm ring-0 transition ease-in-out duration-200"
-              />
-            </button>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </ButtonWithDropdown>
+        <DropdownMenuCheckboxItem
+          v-if="column.can_be_hidden"
+          :model-value="!column.hidden"
+          @update:model-value="onChange(column.key, column.hidden)"
+        >
+          {{ column.label }}
+        </DropdownMenuCheckboxItem>
+      </template>
+    </DropdownMenuContent>
+  </DropdownMenu>
 </template>
 
 <script setup>
-import ButtonWithDropdown from "./ButtonWithDropdown.vue";
-import { usePage } from "@inertiajs/vue3";
-import {computed} from "vue";
+import { computed } from "vue";
+import { Eye } from "lucide-vue-next";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuTrigger,
+    DropdownMenuCheckboxItem,
+} from "./ui/dropdown-menu/index.js";
+import { Button } from "./ui/button/index.js";
 
 const props = defineProps({
     columns: {
@@ -94,12 +52,12 @@ const props = defineProps({
 
     color: {
         type: String,
-        default: 'primary',
+        default: "primary",
         required: false,
     },
 });
 
 const hiddenColumnsCount = computed(() => {
-  return props.columns.filter((c) => c.hidden).length
-})
+    return props.columns.filter((c) => c.hidden).length;
+});
 </script>
